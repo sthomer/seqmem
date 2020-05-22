72b319244c076a074a8204c1e80ac4918dea121b
steven.t.homer@gmail.com <steven.t.homer@gmail.com>
2020-05-23 00:59:00 +0200

    Make HIE recognize test dir by stack build --test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn "Testing not implemented."
+{--
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "Test 1" $
+    context "test 1" $
+      it "is test 1" $
+        1 `shouldBe` 1
+
+main :: IO ()
+main = hspec spec
+--}
