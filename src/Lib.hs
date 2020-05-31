56df30a1dcfb741582c33ae6263f2dfdba32bbfd
steven.t.homer@gmail.com <steven.t.homer@gmail.com>
2020-05-31 18:05:55 +0200

    push token to state

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,7 +12,8 @@ type Token a = [a]
 type Segment a = [Token a]
 type Unigram a = M.Map (Token a) Double
 type Bigram a = M.Map (Token a, Token a) Double
-data Level a = Level { unigram :: Unigram a, bigram :: Bigram a, segment :: Segment a}
+data Level a = Level { unigram :: Unigram a, bigram :: Bigram a, 
+                       segment :: Segment a, prev :: Maybe (Token a) }
 
 run :: String -> IO ()
 run fileName = do
@@ -27,10 +28,13 @@ replaceDigit :: Char -> Char
 replaceDigit x = if isDigit x then '#' else x
 
 level :: Level a
-level = Level { unigram = M.empty, bigram = M.empty, segment = [] }
+level = Level { unigram = M.empty, bigram = M.empty, segment = [], prev = Nothing }
 
 clearSegment :: Level a -> Level a
 clearSegment l = l { segment = [] }
+
+incPrevious :: Level a -> Token a -> Level a
+incPrevious l x = l { prev = Just x }
 
 incSegment :: Level a -> Token a -> Level a
 incSegment l @ Level { segment = xs } x = l { segment = x:xs }
@@ -41,8 +45,20 @@ incUnigram l @ Level { unigram = kvs } x = l { unigram = M.insertWith (+) x 1 kv
 incBigram :: (Ord a) => Level a -> (Token a, Token a) -> Level a
 incBigram l @ Level { bigram = kvs } x = l { bigram = M.insertWith (+) x 1 kvs }
 
-push :: Token a -> State (Level a) ()
-push x = state $ \l -> ((), incSegment l x)
+pushSegment :: Token a -> State (Level a) ()
+pushSegment x = state $ \l -> ((), incSegment l x)
+
+pushUnigram :: (Ord a) => Token a -> State (Level a) ()
+pushUnigram x = state $ \l -> ((), incUnigram l x)
+
+pushBigram :: (Ord a) => Token a -> State (Level a) ()
+pushBigram x = state $ \l @ Level { prev = Just y } -> ((), incBigram l (x, y))
+
+push :: (Ord a) => Token a -> State (Level a) ()
+push x = do 
+  pushBigram x
+  pushUnigram x
+  pushSegment x
 
 flush :: State (Level a) (Segment a)
 flush = state $ \l -> (segment l, clearSegment l)
