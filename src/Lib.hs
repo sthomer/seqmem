a588893e80caf39797ce2d32a5d3c11aae18835f
steven.t.homer@gmail.com <steven.t.homer@gmail.com>
2020-05-31 23:55:59 +0200

    segmentation after counting

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,67 +12,59 @@ type Token a = [a]
 type Segment a = [Token a]
 type Unigram a = M.Map (Token a) Double
 type Bigram a = M.Map (Token a, Token a) Double
-data Level a = Level { unigram :: Unigram a, bigram :: Bigram a, 
-                       segment :: Segment a, prev :: Maybe (Token a) }
+data Level a = Level { unigram :: Unigram a, bigram :: Bigram a }
 
 run :: String -> IO ()
 run fileName = do
   text <- readFile fileName
-  let contents = processText text
+  let contents0 = processText text
+      -- unigram0 = asDist $ countUnigram contents0
+      -- contents1 = flatten $ segmentUnigram unigram0 [] contents0
+      -- unigram1 = asDist $ countUnigram contents1
+      bigram0 = asDist $ countBigram contents0
+      contents1 = flatten $ segmentBigram bigram0 [] contents0
+      bigram1 = asDist $ countBigram contents1
+      contents2 = flatten $ segmentBigram bigram1 [] contents1
+  -- pprint unigram4
+  -- pprint bigram1
+  -- print $ take 100 contents0
+  print $ take 100 contents2
   return ()
 
-processText :: String -> String
-processText = map (replaceDigit . toLower) . filter isAlphaNum .filter isAscii
+processText :: String -> [Token Char]
+processText = map (tokenize . replaceDigit . toLower) . filter isAlphaNum .filter isAscii
 
 replaceDigit :: Char -> Char
 replaceDigit x = if isDigit x then '#' else x
 
+tokenize :: a -> [a]
+tokenize x = [x]
+
 level :: Level a
-level = Level { unigram = M.empty, bigram = M.empty, segment = [], prev = Nothing }
+level = Level { unigram = M.empty, bigram = M.empty }
 
-clearSegment :: Level a -> Level a
-clearSegment l = l { segment = [] }
+pprint :: (Show a, Show b) => M.Map a b -> IO ()
+pprint = mapM_ (putStrLn . \(k, v) -> show k ++ " " ++ show v) . M.toList
 
-incPrevious :: Level a -> Token a -> Level a
-incPrevious l x = l { prev = Just x }
+pairs :: [a] -> [(a, a)]
+pairs xs@(_:ys) = zip xs ys
 
-incSegment :: Level a -> Token a -> Level a
-incSegment l @ Level { segment = xs } x = l { segment = x:xs }
+incUnigram :: (Ord a) => Token a -> Unigram a -> Unigram a
+incUnigram x = M.insertWith (+) x 1
 
-incUnigram :: (Ord a) => Level a -> Token a -> Level a
-incUnigram l @ Level { unigram = kvs } x = l { unigram = M.insertWith (+) x 1 kvs}
+incBigram :: (Ord a) => (Token a, Token a) -> Bigram a -> Bigram a
+incBigram x = M.insertWith (+) x 1
 
-incBigram :: (Ord a) => Level a -> (Token a, Token a) -> Level a
-incBigram l @ Level { bigram = kvs } x = l { bigram = M.insertWith (+) x 1 kvs }
+countUnigram :: (Ord a) => [Token a] -> Unigram a
+countUnigram = foldr incUnigram M.empty
 
-pushSegment :: Token a -> State (Level a) ()
-pushSegment x = state $ \l -> ((), incSegment l x)
-
-pushUnigram :: (Ord a) => Token a -> State (Level a) ()
-pushUnigram x = state $ \l -> ((), incUnigram l x)
-
-pushBigram :: (Ord a) => Token a -> State (Level a) ()
-pushBigram x = state $ \l @ Level { prev = Just y } -> ((), incBigram l (x, y))
-
-push :: (Ord a) => Token a -> State (Level a) ()
-push x = do 
-  pushBigram x
-  pushUnigram x
-  pushSegment x
-
-flush :: State (Level a) (Segment a)
-flush = state $ \l -> (segment l, clearSegment l)
-
-isMoreUnexpected :: (Ord a) => Token a -> Token a -> Unigram a -> Bool
-isMoreUnexpected x y kvs = (kvs M.! x) < (kvs M.! y)
-
-isMoreUncertain :: (Ord a) => Token a -> Token a -> Bigram a -> Bool
-isMoreUncertain x y kvs = entropy (given x kvs) < entropy (given y kvs)
+countBigram :: (Ord a) => [Token a] -> Bigram a
+countBigram = foldr incBigram M.empty . pairs
 
 given :: (Eq a) => Token a -> Bigram a -> Bigram a
 given x = M.filterWithKey (\(z, _) _ -> x == z)
 
-asDist :: Bigram a -> Bigram a
+asDist :: M.Map a Double -> M.Map a Double
 asDist kvs = M.map (/ total) kvs where total = sum $ M.elems kvs
 
 infoContent :: Double -> Double
@@ -81,3 +73,26 @@ infoContent = negate . logBase 2
 entropy :: Bigram a -> Double
 entropy = sum . M.map (\p -> p * infoContent p)
 
+isMoreUnexpected :: (Ord a) => Unigram a -> Token a -> Token a -> Bool
+isMoreUnexpected kvs x y = (kvs M.! x) < (kvs M.! y)
+
+isMoreUncertain :: (Ord a) => Bigram a -> Token a -> Token a -> Bool
+isMoreUncertain kvs x y = entropy (given x kvs) < entropy (given y kvs)
+
+segmentUnigram :: (Ord a) => Unigram a -> [Token a] -> [Token a] -> [[Token a]]
+segmentUnigram kvs s [] = [s]
+segmentUnigram kvs s [_] = [s]
+segmentUnigram kvs s (x:y:xs) = if isMoreUnexpected kvs x y 
+  then reverse (x:s) : segmentUnigram kvs [] (y:xs)
+  else segmentUnigram kvs (x:s) (y:xs)
+
+-- TODO: Too slow => Precompute all entropies
+segmentBigram :: (Ord a) => Bigram a -> [Token a] -> [Token a] -> [[Token a]]
+segmentBigram kvs s [] = [s]
+segmentBigram kvs s [_] = [s]
+segmentBigram kvs s (x:y:xs) = if isMoreUncertain kvs x y
+  then reverse (x:s) : segmentBigram kvs [] (y:xs)
+  else segmentBigram kvs (x:s) (y:xs)
+
+flatten :: [[Token a]] -> [Token a]
+flatten = map concat
